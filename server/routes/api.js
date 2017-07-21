edfb9a65e90153ba450731b1df0411fc76fa8b1f
Victor Leung <vicleung@au1.ibm.com>
2017-07-21 11:11:22 +1000

    updated

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,62 +1,44 @@
 const express = require('express');
 var request = require('request');
-var lib = require('telstrasmsmessagingapilib/lib');
-const oAuthManager = lib.OAuthManager;
-var controller = lib.SMSController;
 
+const https = require('https');
 const router = express.Router();
 
-const errorTitle = 'An error occured!';
-const _credentials = require('../api-credentials');
-
-lib.Configuration.oAuthClientId = _credentials.CLIENT_ID;
-lib.Configuration.oAuthClientSecret = _credentials.CLIENT_SECRET;
-
-lib.Configuration.oAuthTokenUpdateCallback = function (token) {
-    console.log('new token', token);
-};
-
-// called on every request except for /token. Checks that the token is still valid before continuing with the request
-router.use('/', function (req, res, next) {
-    if (oAuthManager.isTokenSet()) {
-        // token is already stored in the client
-        // make API calls as required
-        next();
-    } else {
-        const scopes = [lib.OAuthScopeEnum.SMS];
-        const promise = oAuthManager.authorize(scopes);
-        promise.then((success) => {
-            // client authorized. API calls can be made
-            next();
-        }, (exception) => {
-            // error occurred, exception will be of type lib/Exceptions/OAuthProviderException
-            lib.Configuration.oAuthToken = null;
-            return res.status(exception.errorCode).json({
-                title: `${exception.errorCode} - ${errorTitle}`,
-                message: exception.errorMessage
-            });
-        });
-    }
-});
 
 // sends the SMS object passed in the req.body
 router.post('/send', function (req, res, next) {
-    controller.createSendMessage(req.body, function (error, response, context) {
-        console.log('in post send');
+    var post_options = {
+      host: 'slot2.apipractice.t-dev.telstra.net',
+      port: '443',
+      path: '/v2/messages/sms',
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer aaCH99mGv4sGozXr8PzxF63qZb8o',
+        'Content-Type': 'application/json'
+      }
+    };
 
-        if (error) {
-            return res.status(error.errorCode).json({
-                title: `${error.errorCode} - ${errorTitle}`,
-                message: error.errorMessage
-            });
-        }
-        res.status(200).json({
-            title: 'Success',
-            type: 'success',
-            message: 'Message(s) sent!',
-            obj: response
-        });
+  // Set up the request
+  var post_req = https.request(post_options, (res) => {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('Response: ' + chunk);
     });
+  });
+
+  post_data = {
+    "to": "+61411292236",
+    "body": "test from hackathon",
+    "from": "+61472880109",
+    "validity": 0,
+    "scheduleDelivery": 0,
+    "notifyURL": "string",
+    "replyRequest": true
+  };
+
+  // post the data
+  post_req.write(JSON.stringify(post_data));
+  post_req.end();
 });
 
 // polls the message with messageId
