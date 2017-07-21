f3c7a0bc62f15b9d6281beeddf0b4e0bcb97c0f1
Victor Leung <vicleung@au1.ibm.com>
2017-07-21 10:56:46 +1000

    initial commit

---
+++ server/api-credentials.js
@@ -0,0 +1,7 @@
+// replace these fields with your own credentials
+const _credentials = {
+  CLIENT_ID: '1Pj9lDXkYHWNbfGibDr2sGYPfy2eJfs3',
+  CLIENT_SECRET: 'bcWklLEG2FxRgXio'
+};
+
+module.exports = _credentials;
